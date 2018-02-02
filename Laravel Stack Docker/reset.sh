8b8ef2f654e44da167595e2a2d9005154fb3d0ed
Cyrus <cytmp-github@yahoo.com>
2018-02-02 18:23:12 +0800

    Logic to remove hidden files.

--- Laravel Stack Docker/reset.sh
+++ Laravel Stack Docker/reset.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-docker-compose down -v 
+docker-compose down -v
+umount public_html
+rm -rf public_html
+mkdir public_html
+chmod -R 777 public_html/
+/usr/bin/vmhgfs-fuse .host:/phpcode /root/public_html -o subtype=vmhgfs-fuse,allow_other
+sleep 2s
 rm -rf public_html/*
-chmod -R 777 public_html
+find public_html/ -type f -name ".*" -delete
+chmod -R 777 public_html/
 rm -rf data-mysql
 mkdir data-mysql
 chmod 777 data-mysql/
