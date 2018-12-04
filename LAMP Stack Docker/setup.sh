7483344c63442fca08dc1487a622c68d39c1d002
EC2 Default User <ec2-user@ip-172-31-10-49.ap-southeast-1.compute.internal>
2018-12-04 02:18:22 +0000

    fix wrong cidr and script to get ip.

--- LAMP Stack Docker/setup.sh
+++ LAMP Stack Docker/setup.sh
@@ -2,5 +2,5 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 security_group=$(ec2-metadata -s | cut -d " " -f 2);
-aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 80 --cidr 0.0.0.0/32
-aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 8080 --cidr 0.0.0.0/32
+aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 80 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 8080 --cidr 0.0.0.0/0
