8ebae87a0e18a57df29ea38cd12025bdaaacd7db
EC2 Default User <ec2-user@ip-172-31-15-73.ap-southeast-1.compute.internal>
2018-12-04 02:04:59 +0000

    changed for Cloud9 with new script to wrap docker command.

---
+++ LAMP Stack Docker/reset.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker-compose down -v
+sudo rm -rf data-mysql
+mkdir data-mysql
---
+++ LAMP Stack Docker/setup.sh
@@ -0,0 +1,6 @@
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+security_group=$(ec2-metadata -s | cut -d " " -f 2);
+aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 80 --cidr 0.0.0.0/32
+aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 8080 --cidr 0.0.0.0/32
