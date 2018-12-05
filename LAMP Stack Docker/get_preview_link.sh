f617aac3ada22868ef99e033694e6a26abec017c
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2018-12-05 14:31:06 +0800

    Add hostname.

--- LAMP Stack Docker/get_preview_link.sh
+++ LAMP Stack Docker/get_preview_link.sh
@@ -3,3 +3,8 @@ echo "Web Server"
 echo "http://$ip"
 echo "PhpMyAdmin"
 echo "http://$ip:8080"
+hostname=$(curl http://169.254.169.254/latest/meta-data/local-hostname)
+echo "Web Server Host"
+echo "http://$hostname"
+echo "PhpMyAdmin Host"
+echo "http://$hostname:8080"
