b69964374c49bcfeba6b6bba0eec82b9417f92a5
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2018-12-05 14:15:05 +0800

    Create public_html folder with permission.

--- LAMP Stack Docker/setup.sh
+++ LAMP Stack Docker/setup.sh
@@ -4,3 +4,5 @@ docker-compose --version
 security_group=$(ec2-metadata -s | cut -d " " -f 2);
 aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 80 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 8080 --cidr 0.0.0.0/0
+mkdir public_html
+chmod 777 public_html
