9f827307fdf046d78d0a6364ad3eb2b69336983f
EC2 Default User <ec2-user@ip-172-31-63-56.ec2.internal>
2019-02-14 09:25:06 +0000

    create a symbolic link to /usr/bin and updated to 1.23.2.

--- LAMP Stack Docker/setup.sh
+++ LAMP Stack Docker/setup.sh
@@ -1,5 +1,6 @@
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
 security_group=$(ec2-metadata -s | cut -d " " -f 2);
 aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 80 --cidr 0.0.0.0/0
